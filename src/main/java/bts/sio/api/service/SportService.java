ceb14a9641a9295d6a169023d7a1057ab8659dd1
FlorianRichardd <florian.richardb3d@gmail.com>
2023-09-20 10:49:36 +0200

    Création Sport Service

---
+++ src/main/java/bts/sio/api/service/SportService.java
@@ -0,0 +1,34 @@
+package bts.sio.api.service;
+
+import lombok.Data;
+import bts.sio.api.model.Sport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import bts.sio.api.repository.SportRepository;
+
+import java.util.Optional;
+@Data
+@Service
+
+public class SportService {
+    @Autowired
+    private SportRepository sportRepository;
+
+    public Optional<Sport> getSport(final Long id) {
+        return sportRepository.findById(id);
+    }
+
+    public Iterable<Sport> getSport() {
+        return sportRepository.findAll();
+    }
+
+    public void deleteSport(final Long id) {
+        sportRepository.deleteById(id);
+    }
+
+    public Sport saveSport(Sport sport) {
+        Sport savedSport = sportRepository.save(sport);
+        return savedSport;
+    }
+}
+
